da8b481219345daadea3f162ab45c87dbe2a91fd
Alain Petit <alainpetit@hotmail.com>
2018-10-07 17:13:32 -0400

    Added handling of Command Design Pattern

--- src/Controller/CryptoFinanceUpdater.java
+++ src/Controller/CryptoFinanceUpdater.java
@@ -1,10 +1,23 @@
 package Controller;
 
+import Model.IModelCountainer;
+import Model.ModelFactory;
 import View.JFrame_Main;
+
 
 public class CryptoFinanceUpdater implements IControllerApp{
     static private CryptoFinanceUpdater mObjController;  
+    static private IModelCountainer mObjModelCountainer;  
     private JFrame_Main mFrmMain;
+
+	@Override
+	public void notify(AppCommandMessage msg) {
+		/* Ideally This should be a 2-phase process, notify should place in some sort of list, 
+		 * and then another thread should parse and process the msg.execute();
+		 * But for sake of simplicity, this will be a direct execute for now. 
+		 */ 
+		msg.execute(this, mObjModelCountainer, mFrmMain);
+	}
 
     public static void main(String[] args) {
         /* Set the Nimbus look and feel */
@@ -32,10 +45,12 @@ public class CryptoFinanceUpdater implements IControllerApp{
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> {
-                mObjController= new CryptoFinanceUpdater();
+            mObjController= new CryptoFinanceUpdater();
+            mObjModelCountainer= ModelFactory.getModelCountainer();
 
-                mObjController.mFrmMain = new JFrame_Main();
-                mObjController.mFrmMain.setVisible(true);
+            mObjController.mFrmMain = new JFrame_Main();
+			mObjController.mFrmMain.setController(mObjController);
+            mObjController.mFrmMain.setVisible(true);
         });
     }
 }
